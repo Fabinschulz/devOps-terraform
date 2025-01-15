6f379c38465d1e25bd2e960357a3d987ef75a84e
fabio lima <fabio.lima19997@gmail.com>
2025-01-14 23:58:16 -0300

    feat: terraform state remote

--- providers.tf
+++ providers.tf
@@ -5,6 +5,30 @@ terraform {
       version = "5.68.0"
     }
   }
+  # backend "s3" {
+  #   bucket  = "synthra-state-stg"
+  #   region  = "us-east-2"
+  #   key     = "terraform.tfstate"
+  #   encrypt = true
+  # }
 }
 
 provider "aws" {}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.bucket_state
+
+  lifecycle {
+    prevent_destroy = true // Previne a exclusão do bucket
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  depends_on = [aws_s3_bucket.tf_state]
+}
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "bucket_state" {
+  type        = string
+  default     = "synthra-state-stg"
+  description = "Bucket name for the state file"
+}
